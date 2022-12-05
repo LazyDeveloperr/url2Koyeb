26544ddf0dc1ca8b65cc364e1df13b9e7503d6a4
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-05 20:33:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,10 +14,9 @@ if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(root="plugins"),
     Warrior = LazyDeveloper("@LazyDeveloper",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
-    plugins=plugins)
+    plugins={"root": "plugins"})
     Warrior.run()
