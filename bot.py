bda166247e256ee2b066a402a8467e76e682c390
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-06 18:48:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="URLBOT",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
