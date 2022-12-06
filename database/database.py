5b6beaa3f78419a854b0f8a1561cf704db1f672d
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-06 18:05:15 +0530

    Add files via upload

--- database/database.py
+++ database/database.py
@@ -7,8 +7,8 @@ class Database:
 
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.clinton = self._client[database_name]
-        self.col = self.clinton.USERS
+        self.db = self._client[database_name]
+        self.col = self.db.users
 
     def new_user(self, id):
         return dict(id=id, thumbnail=None)
